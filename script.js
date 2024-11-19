ebb6c0932a949490475aea9fdf47fb0bbf39e25f
deebaaga <deebagamushtaq@gmail.com>
2024-11-19 22:58:24 +0530

    did some random fixes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Add Smooth Scrolling Effect
 document.querySelectorAll('a[href^="#"]').forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    target.scrollIntoView({ behavior: "smooth", block: "start" });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
 });
